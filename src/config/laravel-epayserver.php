51a96d3c995e45e35cd0b1d6466a1bb6b3715eeb
Jose Galindo <66330140+jgalindosl@users.noreply.github.com>
2022-06-22 17:55:44 -0600

    update variable names

--- src/config/laravel-epayserver.php
+++ src/config/laravel-epayserver.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'test'         => env('LARAVEL_EPAY_SERVER_TEST', true),
-    'affilliation' => env('LARAVEL_EPAY_SERVER_AFFILLIATION'),
-    'terminal'     => env('LARAVEL_EPAY_SERVER_TERMINAL'),
-    'user'         => env('LARAVEL_EPAY_SERVER_USER'),
-    'password'     => env('LARAVEL_EPAY_SERVER_PASSWORD'),
+    'test'         => env('EPAY_SERVER_TEST', true),
+    'affilliation' => env('EPAY_SERVER_AFFILLIATION'),
+    'terminal'     => env('EPAY_SERVER_TERMINAL'),
+    'user'         => env('EPAY_SERVER_USER'),
+    'password'     => env('EPAY_SERVER_PASSWORD'),
 ];
