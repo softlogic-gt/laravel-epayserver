ce8da2b2bef8d3b5ef1f84db8451820f2059f556
Jose Galindo <66330140+jgalindosl@users.noreply.github.com>
2022-06-22 17:28:14 -0600

    sale complete

--- README.md
+++ README.md
@@ -6,4 +6,18 @@
 
 ## Usage
 
-`$epay = new `
+`
+use SoftlogicGT\LaravelEpayServer\LaravelEpayServer;
+
+$creditCard = '4000000000000416';
+$expirationMonth = '2';
+$expirationYear = '26';
+$cvv2 = '123';
+$amount = 1230.00;
+$externalId = '557854';
+
+$response = LaravelEpayServer::sale($creditCard, $expirationMonth, $expirationYear, $cvv2, $amount, $externalId);
+`
+It will throw an exception if any error is received from Epay Server, or an object with the following info:
+
+`[[auditNumber] => 111111 [referenceNumber] => 254555555 [authorizationNumber] => 022226 [responseCode] => 00 [messageType] => 0210]`
--- src/LaravelEpayServer.php
+++ src/LaravelEpayServer.php
@@ -1,22 +1,118 @@
 <?php
 namespace SoftlogicGT\LaravelEpayServer;
 
+use SoapFault;
 use SoapClient;
+use Illuminate\Support\Facades\Validator;
 
 class LaravelEpayServer
 {
-    public static function sale()
+    protected static $codes = [
+        "00" => "Aprobada",
+        "01" => "Refiérase al Emisor",
+        "02" => "Refiérase al Emisor",
+        "05" => "Transacción No Aceptada",
+        "12" => "Transacción Inválida",
+        "13" => "Monto Inválido",
+        "19" => "Transacción no realizada, intente de nuevo 31 Tarjeta no soportada por switch",
+        "35" => "Transacción ya ha sido ANULADA",
+        "36" => "Transacción a ANULAR no EXISTE",
+        "37" => "Transacción de ANULACION REVERSADA",
+        "38" => "Transacción a ANULAR con Error",
+        "41" => "Tarjeta Extraviada",
+        "43" => "Tarjeta Robada",
+        "51" => "No tiene fondos disponibles",
+        "57" => "Transacción no permitida",
+        "58" => "Transacción no permitida en la terminal",
+        "65" => "Límite de actividad excedido",
+        "80" => "Fecha de Expiración inválida",
+        "89" => "Terminal inválida",
+        "91" => "Emisor no disponible",
+        "94" => "Transacción duplicada",
+        "96" => "Error del sistema, intente más tarde",
+    ];
+
+    public static function sale($creditCard, $expirationMonth, $expirationYear, $cvv2, $amount, $externalId)
     {
-        return self::common();
+        return self::common($creditCard, $expirationMonth, $expirationYear, $cvv2, $amount, $externalId, '0200');
     }
 
-    protected static function common()
+    protected static function common($creditCard, $expirationMonth, $expirationYear, $cvv2, $amount, $externalId, $messageType)
     {
-        $url = config('laravel-epayserver.test') ? 'https://epaytestvisanet.com.gt/?wsdl' : 'https://epayvisanet.com.gt/?wsdl';
+        $data = compact("creditCard", "expirationMonth", "expirationYear", "cvv2", "amount", "externalId", "messageType");
 
+        $rules = [
+            'cvv2'            => 'required|min:3|max:4',
+            'expirationMonth' => 'required|numeric|lte:12|gte:1',
+            'expirationYear'  => 'required|numeric|lte:99|gte:1',
+            'amount'          => 'required|numeric',
+            'externalId'      => 'required',
+            'messageType'     => 'required',
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            abort(422, json_encode(['errors' => $validator->errors()]));
+        }
+
+        $month = str_pad($expirationMonth, 2, "0", STR_PAD_LEFT);
+        $year  = str_pad($expirationYear, 2, "0", STR_PAD_LEFT);
+
+        $url        = config('laravel-epayserver.test') ? 'https://epaytestvisanet.com.gt/?wsdl' : 'https://epayvisanet.com.gt/?wsdl';
         $soapClient = new SoapClient($url, ["trace" => 1]);
-        $res        = $soapClient->AuthorizationRequest([]);
+        $params     = [
+            'AuthorizationRequest' => [
+                'posEntryMode'     => '012',
+                'pan'              => $creditCard,
+                'expdate'          => $year . $month,
+                'amount'           => ($amount * 100),
+                'cvv2'             => $cvv2,
+                'paymentgwIP'      => '190.149.69.135',
+                'shopperIP'        => '190.149.168.54',
+                'merchantServerIP' => '67.205.167.98',
+                'merchantUser'     => config('laravel-epayserver.user'),
+                'merchantPasswd'   => config('laravel-epayserver.password'),
+                'merchant'         => config('laravel-epayserver.affilliation'),
+                'terminalId'       => config('laravel-epayserver.terminal'),
+                'messageType'      => $messageType,
+                'auditNumber'      => $externalId,
+                'additionalData'   => '',
+            ],
+        ];
 
-        return $res->AuthorizationResponse;
+        print_r($params);
+        try {
+            $res  = $soapClient->AuthorizationRequest($params);
+            $code = $res->response->responseCode;
+            //If succesful response, return full response
+            if ($code == '00') {
+                return $res->response;
+            }
+            //If error, return error from list or unknown
+            if (array_key_exists($code, self::$codes)) {
+                abort(400, self::$codes[$code]);
+            }
+            abort(400, "Error desconocido: " . $code);
+
+        } catch (SoapFault $exception) {
+            echo '<h2>exception</h2>';
+            print_r($exception);
+            echo '<br/><h2>exception trace</h2>';
+            var_dump($exception->getTraceAsString());
+
+            echo '<br/>Request headers : <br/><xmp>',
+            $soapClient->__getLastRequestHeaders(),
+                '</xmp><br/>';
+            echo 'Request : <br/><xmp>',
+            $soapClient->__getLastRequest(),
+                '</xmp><br/>';
+            echo 'Response headers: <br/><xmp>',
+            $soapClient->__getLastResponseHeaders(),
+                '</xmp><br/>';
+            echo 'Response : <br/><xmp>',
+            $soapClient->__getLastResponse(),
+                '</xmp><br/>';
+        }
     }
 }
